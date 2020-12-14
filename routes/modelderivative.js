6b329d9a5a6a148648b754dce9d6802f1be7e47d
Михаил Строганов 171-334 <46332122+Shrek4@users.noreply.github.com>
2020-12-14 23:51:54 +0300

    Update modelderivative.js

--- routes/modelderivative.js
+++ routes/modelderivative.js
@@ -1,19 +1,19 @@
 /////////////////////////////////////////////////////////////////////
-// Copyright (c) Autodesk, Inc. All rights reserved
-// Written by Forge Partner Development
+// Авторское право (c) Autodesk, Inc. Все права защищены.
+// Автор Forge Partner Development
 //
-// Permission to use, copy, modify, and distribute this software in
-// object code form for any purpose and without fee is hereby granted,
-// provided that the above copyright notice appears in all copies and
-// that both that copyright notice and the limited warranty and
-// restricted rights notice below appear in all supporting
-// documentation.
+// Разрешение на использование, копирование, изменение и распространение этого программного обеспечения в
+// форма объектного кода для любых целей и без комиссии предоставляется,
+// при условии, что указанное выше уведомление об авторских правах присутствует во всех копиях и
+// что это уведомление об авторских правах и ограниченная гарантия и
+// примечание об ограниченных правах ниже появляется во всех поддерживающих
+// документация.
 //
-// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
-// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
-// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
-// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
-// UNINTERRUPTED OR ERROR FREE.
+// AUTODESK ПРЕДОСТАВЛЯЕТ ДАННУЮ ПРОГРАММУ "КАК ЕСТЬ" И СО ВСЕМИ ОШИБКАМИ.
+// AUTODESK ОТКАЗЫВАЕТСЯ ОТ ЛЮБЫХ ПОДРАЗУМЕВАЕМЫХ ГАРАНТИЙ
+// КОММЕРЧЕСКАЯ ЦЕННОСТЬ ИЛИ ПРИГОДНОСТЬ ДЛЯ ОПРЕДЕЛЕННОГО ИСПОЛЬЗОВАНИЯ. АВТОДЕСК, ИНК.
+// НЕ ГАРАНТИРУЕТ, ЧТО РАБОТА ПРОГРАММЫ БУДЕТ
+// БЕСПЕРЕБОЙНЫЙ ИЛИ БЕЗ ОШИБОК.
 /////////////////////////////////////////////////////////////////////
 
 const express = require('express');
@@ -29,7 +29,7 @@ const { getClient, getInternalToken } = require('./common/oauth');
 
 let router = express.Router();
 
-// Middleware for obtaining a token for each request.
+// Промежуточное ПО для получения токена для каждого запроса.
 router.use(async (req, res, next) => {
     const token = await getInternalToken();
     req.oauth_token = token;
@@ -37,8 +37,8 @@ router.use(async (req, res, next) => {
     next();
 });
 
-// POST /api/forge/modelderivative/jobs - submits a new translation job for given object URN.
-// Request body must be a valid JSON in the form of { "objectName": "<translated-object-urn>" }.
+// POST /api/forge/modelderivative/jobs - отправляет новое задание на перевод для данного объекта URN.
+// Тело запроса должно быть корректным JSON в форме {"objectName": "<translated-object-urn>"}.
 router.post('/jobs', async (req, res, next) => {
     let job = new JobPayload();
     job.input = new JobPayloadInput();
@@ -49,7 +49,7 @@ router.post('/jobs', async (req, res, next) => {
     job.output.formats[0].type = 'svf';
     job.output.formats[0].views = ['2d', '3d'];
     try {
-        // Submit a translation job using [DerivativesApi](https://github.com/Autodesk-Forge/forge-api-nodejs-client/blob/master/docs/DerivativesApi.md#translate).
+        // Отправляем задание на перевод, используя [DerivativesApi](https://github.com/Autodesk-Forge/forge-api-nodejs-client/blob/master/docs/DerivativesApi.md#translate).
         await new DerivativesApi().translate(job, {}, req.oauth_client, req.oauth_token);
         res.status(200).end();
     } catch(err) {
